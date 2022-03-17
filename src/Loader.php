a737309a47df1cba23027971a7320d9387b41f28
Ngọc Lam <nglamvn2911@gmail.com>
2022-03-17 17:58:45 +0700

    Something ?

---
+++ src/Loader.php
@@ -0,0 +1,13 @@
+<?php
+declare(strict_types=1);
+
+namespace lmao;
+
+use pocketmine\plugin\PluginBase;
+
+class Loader extends PluginBase{
+
+	protected function onEnable() : void{
+		//TODO: onEnable
+	}
+}
